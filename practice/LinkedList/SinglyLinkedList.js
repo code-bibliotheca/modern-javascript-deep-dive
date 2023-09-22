839c933262c0a3fb535230bacbc2c9277249d683
bubobubobo <phyzee@naver.com>
2023-09-23 07:22:15 +0900

    feat : SinglyLinkedList 파일만 추가

---
+++ practice/LinkedList/SinglyLinkedList.js
@@ -0,0 +1,15 @@
+class Node {
+  #value;
+  #next;
+
+  constructor(value, next) {
+    this.#value = value;
+    this.#next = next;
+  }
+}
+
+class SinglyLinkedList {
+  // do something...!
+}
+
+export default SinglyLinkedList;
---
+++ practice/LinkedList/SinglyLinkedList.test.js
@@ -0,0 +1,11 @@
+import SinglyLinkedList from "./SinglyLinkedList";
+
+describe("SinglyLinkedList", () => {
+  let linkedList;
+
+  beforeEach(() => {
+    linkedList = new SinglyLinkedList();
+  });
+
+  // TODO : 내용 추가
+});
